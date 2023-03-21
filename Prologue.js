679bcbdcd6093fee92caee3f41e3851510406d1e
iiaky <serenayung2005@gmail.com>
2023-03-20 20:33:00 -0400

    aslkjdflksdajf

--- Prologue.js
+++ Prologue.js
@@ -1,10 +1,20 @@
 class Prologue {
-    constructor() {
-
+    constructor( {scene, onComplete }) { // maybe I specify which cutscene to use through the constructor?   
+                    // so that I control what is shown outside of this class, and this class only handles actually showing it
+        this.scene = scene; // "event1", "event2", etc
+        this.onComplete = onComplete;
     }
 
     getNext() {
         // get the next scene in the cutscene
+        const event = window.cutscenes[this.scene]; // this will be each "episode", if you will
+        for (let i = 0; i < event.length; i++) {
+
+        }
+        if (text.length === 0) {
+            src = key1[next].src
+            text = key1[next].events
+        }
     }
 
     createElement() {
@@ -15,12 +25,13 @@ class Prologue {
     init() {
         this.createElement();
         container.appendChild(this.element);
+        this.getNext();
     }
 
 }
 
 window.cutscenes = {
-    key1: [ // each specified cutscene
+    event1: [ // each specified cutscene
         {   
             required: ["pp1"],
             src: "images/cutscenes/prologue/testprologue1.png",
@@ -35,6 +46,9 @@ window.cutscenes = {
             ]
         }
         
+    ],
+    event2: [
+
     ]
     
 }
